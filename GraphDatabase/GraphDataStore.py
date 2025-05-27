289519e74aa26d07ee3d7e1ce66976b8d4457758
DrBill688 <drbill688@gmail.com>
2025-05-26 22:41:14 -0400

    Initial

---
+++ GraphDatabase/GraphDataStore.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@author: William N. Roney, ScD
+"""
+from typing import Self
+import pandas, json
+
+class GraphDataStore:
+    def __init__(self):
+        self.sources = pandas.DataFrame(columns=['SourceRef'])
+        self.model = pandas.DataFrame(columns=['ParentType', 'ChildType', 'SourceRef'])
+        self.edges = pandas.DataFrame(columns=['ParentType', 'ParentValue', 'ChildType', 'ChildValue', 'SourceRef'])
+        return
+    def __str__(self) -> str:
+        return f'Sources:\n{self.sources}\nModel:\n{self.model}\nEdges:\n{self.edges}'
+    def addSource(self, source: str) -> Self:
+        if len(self.sources[self.sources['SourceRef'] == source]) == 0:
+            self.sources.loc[len(self.sources)] = [source]
+        return self
+    def addModel(self, pt, ct, sr)-> Self:
+        matching_row = self.model[(self.model['ParentType'] == pt) & (self.model['ChildType'] == ct)] 
+        if len(matching_row) == 0:
+            self.model.loc[len(self.model)] = [pt, ct, sr]
+        else:
+            existing_sr = matching_row['SourceRef'].iloc[0]
+            try:
+                existing_sr = json.loads(existing_sr)
+            except:
+                existing_sr = [existing_sr]
+            existing_sr.append(sr)
+            self.model.loc[(self.model['ParentType'] == pt) & (self.model['ChildType'] == ct), 'SourceRef'] = json.dumps(existing_sr)
+        return self
+    
